2d4d7972a2ef92728e7b8c82ec02afc29edde22a
jiangshanmeta <540118044@qq.com>
2016-04-20 21:47:25 +0800

    hashtable

--- myWork/dataStructureAndAlgorithms/hashtable.js
+++ myWork/dataStructureAndAlgorithms/hashtable.js
@@ -7,16 +7,51 @@ function HashTable(){
 		}
 		return hash;
 	}
+	var ValuePair = function(key,value){
+		this.key = key;
+		this.value = value;
+	}
 	this.put = function(key,value){
 		var pos = loseloseHashCode(key);
-		table[pos] = value;
+		var ele = new ValuePair(key,value);
+		if(table[pos] == undefined){
+			table[pos] = ele;
+		}else{
+			pos++;
+			while(table[pos] != undefined){
+				pos++;
+			}
+			table[pos] = ele;
+		}
 	}
 	this.remove = function(key){
 		var pos = loseloseHashCode(key);
-		table[pos] = undefined; 
+		if(table[pos] == undefined){return;}
+		if(table[pos].key == key){
+			 table[pos] = undefined;
+		}else{
+			pos++;
+			while(table[pos] == undefined || table[pos].key != key ){
+				pos++;
+			}
+			table[pos] = undefined;
+		}
 	}
 	this.get = function(key){
 		var pos = loseloseHashCode(key);
-		return table[pos];
+		if(table[pos] == undefined){return;}
+		if(table[pos].key == key){
+			return table[pos].value;
+		}else{
+			pos++;
+			while(table[pos] == undefined || table[pos].key != key ){
+				pos++;
+			}
+			if(table[pos] && table[pos].key == key){
+				return table[pos].value;
+			}
+		}
+		return undefined;
+
 	}
 }
