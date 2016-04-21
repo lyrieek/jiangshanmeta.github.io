c9667639deacc1a1ca99b9f7b00fdc476fe7f2f6
jiangshanmeta <540118044@qq.com>
2016-04-21 11:50:06 +0800

    Array.from array.prototype.includes array.prototype.find array.prototype.findIndex

--- myWork/ECMAScript6/js/array.js
+++ myWork/ECMAScript6/js/array.js
@@ -27,4 +27,31 @@ console.log(arr4);
 var arr5 = Array.of(3,11,8);
 console.log(arr5,"arr5");
 
+//这个方法似乎没什么用
 var arr6 = [1,2,3,4,5];
+arr6.copyWithin(1,0,2);
+console.log(arr6);
+
+//数组遍历的方法,find返回第一个满足条件的值，findIndex返回第一个满足条件的值的索引
+var arr7 = [1,4,-5,10];
+var firstOne = arr7.find(function(item,index,arr){
+	return item>9;
+})
+console.log(firstOne);
+
+var firstIndx = arr7.findIndex(function(item,index,arr){
+	return item == -5;
+})
+console.log(firstIndx);
+
+for(let index of arr7.keys()){
+	console.log(index);
+}
+console.log(Array.prototype.keys);
+console.log(Array.prototype.values);//我的浏览器暂不支持values
+// for(let ele of arr7.values()){
+// 	console.log(ele);
+// }
+
+//类似于字符串的includes，作为indexOf的替代品
+console.log(arr7.includes(0))
