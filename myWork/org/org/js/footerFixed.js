ff7c69c0fa94590ae96cc94b7e48491594e69aa9
jiangshanmeta <540118044@qq.com>
2016-05-11 23:20:53 +0800

    blog -1

--- myWork/org/org/js/footerFixed.js
+++
@@ -1,7 +0,0 @@
-$.fn.footerFixed = function(){
-	var windowH = $(window).height();
-	var bodyH = $("body").height();
-	if(bodyH<windowH){
-		$(this).css({position:'fixed','bottom':'0','width':'100%'});
-	}
-}
