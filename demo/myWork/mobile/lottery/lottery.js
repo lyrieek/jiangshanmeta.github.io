d3f8039ac356b74e534d0fc79dc3d7ee74ad7bcc
jiangshanmeta <540118044@qq.com>
2016-09-05 19:04:58 +0800

    modify

--- demo/myWork/mobile/lottery/lottery.js
+++ demo/myWork/mobile/lottery/lottery.js
@@ -30,6 +30,36 @@ if (!Object.assign) {
     }
   });
 }
+// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Object/create
+if (typeof Object.create != 'function') {
+  Object.create = (function() {
+    function Temp() {}
+    var hasOwn = Object.prototype.hasOwnProperty;
+
+    return function (O) {
+      if (typeof O != 'object') {
+        throw TypeError('Object prototype may only be an Object or null');
+      }
+
+      Temp.prototype = O;
+      var obj = new Temp();
+      Temp.prototype = null; 
+
+      if (arguments.length > 1) {
+
+        var Properties = Object(arguments[1]);
+        for (var prop in Properties) {
+          if (hasOwn.call(Properties, prop)) {
+            obj[prop] = Properties[prop];
+          }
+        }
+      }
+
+      return obj;
+    };
+  })();
+}
+
 //impress.js
 var pfx = (function () {
     var style = document.createElement('dummy').style,
