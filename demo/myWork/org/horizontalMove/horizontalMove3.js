078f7a20a022ff348c892e7518b5a6b725378c58
jiangshanmeta <540118044@qq.com>
2016-10-24 23:23:03 +0800

    1

---
+++ demo/myWork/org/horizontalMove/horizontalMove3.js
@@ -0,0 +1,18 @@
+(function(window){
+	var defaults = {
+
+	}
+	function HorizontalMove(opt){
+
+	}
+
+	HorizontalMove.prototype = {
+		constructor:HorizontalMove,
+		bindEvent:function(){
+
+		},
+
+	}
+	window.HorizontalMove = HorizontalMove;
+
+})(window);
