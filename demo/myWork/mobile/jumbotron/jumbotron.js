1d7175697b0e36fced7a71aa66715de982f542e4
jiangshanmeta <540118044@qq.com>
2017-01-31 17:50:34 +0800

    jumbotron update with widget as base class

--- demo/myWork/mobile/jumbotron/jumbotron.js
+++ demo/myWork/mobile/jumbotron/jumbotron.js
@@ -1,6 +1,6 @@
 (function(window){
+	// 想实现一个单例
 	var instance = null;
-	var status = null;
 	var defaults = {
 		selector:"#jumbotron",
 		openFunc:'linear',
@@ -27,14 +27,14 @@
 	    if(!(instance instanceof Jumbotron)){
 	    	// 初始化设置
 	    	this.option = Object.assign({},defaults,option||{});
-	    	this.init();
+	    	this.initDOM();
 	    	instance = this;
 	    }
 	    return instance;
 	}
-	Jumbotron.prototype = {
+	Jumbotron.prototype = Object.assign({},Widget.prototype,{
 		constructor:Jumbotron,
-		init:function(){
+		initDOM:function(){
 			this.ele = document.querySelector(this.option.selector);
 			if(!this.ele){
 				console.error('missing parameter');
@@ -42,42 +42,57 @@
 			}
 			this.oriDisplay = window.getComputedStyle(this.ele)['display'];
 			this.ele.style.display = "none";
-			status = 0;
+			this.status = 0;
 			var _this = this;
 			this.ele.addEventListener(whichTransitionEvent(),function(){
-				if(status==1){
-					status = 2;
+				if(_this.status==1){
+					_this.status = 2;
 					_this.ele.style.height = "auto";
-					gettype(_this.option.doSthAfterOpen)==='function' && _this.option.doSthAfterOpen();
-				}else if(status==3){
-					status = 4;
+					_this.fire('afterOpen');
+				}else if(_this.status==3){
+					_this.status = 4;
 					_this.ele.style.display = "none";
-					gettype(_this.option.doSthAfterClose)==='function' && _this.option.doSthAfterClose();
+					_this.fire('afterClose');
 				}
 			});
+			if(gettype(this.option.doSthBeforeOpen)==='function'){
+				this.on('beforeOpen',this.option.doSthBeforeOpen);
+			}
+			if(gettype(this.option.doSthAfterOpen)==='function'){
+				this.on('afterOpen',this.option.doSthAfterOpen);
+			}
+
+			if(gettype(this.option.doSthBeforeClose)==='function'){
+				this.on('beforeClose',this.option.doSthBeforeClose);
+			}
+			if(gettype(this.option.doSthAfterClose)==='function'){
+				this.on('afterClose',this.option.doSthAfterClose);
+			}			
+
 		},
 		open:function(){
-			if(status!==0){
+			if(this.status!==0){
 				return;
 			}
-			gettype(this.option.doSthBeforeOpen)==='function' && this.option.doSthBeforeOpen();
+			this.fire('beforeOpen');
 			this.ele.style[pfx('transition')] = "height " + this.option.openDuration + "ms "+ this.option.openFunc;
 			this.ele.style.cssText += "display:"+ this.oriDisplay + ";height:0;overflow:hidden;"
 			this.ele.style.height = this.ele.scrollHeight + 'px';
-			status = 1;
+			this.status = 1;
 		},
 		close:function(){
-			if(status!=2){
+			if(this.status!=2){
 				return;
 			}
-			gettype(this.option.doSthBeforeClose)==='function' && this.option.doSthBeforeClose();
+			this.fire('beforeClose');
 			this.ele.style.height = getComputedStyle(this.ele)['height'];
 			this.ele.style[pfx('transition')] = "height " + this.option.closeDuration + "ms "+ this.option.closeFunc;
-			status = 3;
+			this.status = 3;
 			getComputedStyle(this.ele)['height'];
 			this.ele.style.height = 0;
 		}
-	}
+	});
+
 	window.jumbotron = Jumbotron;
 	window.Jumbotron = Jumbotron;
 })(window);
